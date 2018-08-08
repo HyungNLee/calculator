1754c76ab463ba969b1a7859cb6a086d50b22b6e
zhuleqiang and Hyung Lee <zhuleqiang@>
2018-08-08 10:56:18 -0700

    Add buttons for subtract/multipy/divide functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,4 @@
-//prompt for number inputs.
-var number1 = parseFloat(prompt("Enter a number:"));
-var number2 = parseFloat(prompt("Enter another number:"));
-
+//Business (or back-end) logic:
 //Add function
 var add = function(number1, number2) {
 	return number1 + number2;
@@ -18,10 +15,34 @@ var divide = function(number1, number2) {
 };
 
 //Multiply function
-var multiplication = function(number1, number2) {
+var multiply = function(number1, number2) {
   return number1 * number2;
 };
 
-//Shows result in alert function.
-result = multiplication(number1, number2);
-alert(result);
+// Everything below this line is user interface (or front-end) logic:
+
+$(document).ready(function() {
+
+	$("#numberBox button").click(function() {
+		event.preventDefault();
+		var number1 = parseInt($("#number1").val());
+		var number2 = parseInt($("#number2").val());
+		if($(this).attr("id")=="add") {
+			var result = add(number1, number2);
+			$("#output").text(result);
+			$("#numberBox")[0].reset();
+		} else if($(this).attr("id")=="subtract") {
+			var result = subtract(number1, number2);
+			$("#output").text(result);
+			$("#numberBox")[0].reset();
+		} else if($(this).attr("id")=="divide") {
+			var result = divide(number1, number2);
+			$("#output").text(result);
+			$("#numberBox")[0].reset();
+		} else if ($(this).attr("id")=="multiply") {
+			var result = multiply(number1, number2);
+			$("#output").text(result);
+			$("#numberBox")[0].reset();
+		}
+	})
+})
